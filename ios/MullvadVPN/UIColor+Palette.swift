4f989c6e3240acab567b1c80f1e553cb9b44b1b4
Andrej Mihajlov <and@mullvad.net>
2021-10-08 14:43:19 +0200

    Palette: add TextField.invalidInputTextColor

--- ios/MullvadVPN/UIColor+Palette.swift
+++ ios/MullvadVPN/UIColor+Palette.swift
@@ -33,6 +33,7 @@ extension UIColor {
     enum TextField {
         static let placeholderTextColor = UIColor(red: 0.16, green: 0.30, blue: 0.45, alpha: 0.40)
         static let textColor = UIColor(red: 0.16, green: 0.30, blue: 0.45, alpha: 1.00)
+        static let invalidInputTextColor = UIColor.dangerColor
     }
 
     enum AppButton {
